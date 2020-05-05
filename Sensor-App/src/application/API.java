f4813b455da40b32edcb8507e4d3e4a9a6e81a8d
SBA <buddikasamesh1@gmail.com>
2020-05-05 19:59:07 +0530

    adding comments for sensor app

--- Sensor-App/src/application/API.java
+++ Sensor-App/src/application/API.java
@@ -9,18 +9,26 @@ import org.json.JSONArray;
 
 public class API 
 {
-	public boolean checkSensorById(String id)
+    //check whether a sensor is avaiable by the id which is passed as a parameter
+    //and returning a boolean.
+ 	public boolean checkSensorById(String id)
 	{
 		try
 		{
+		    //this is the url to check whther a sensor is avaiable or not
 			String burl = "http://localhost:5000/api/sensors/findSensorById?id=" +id;
+			//and replacing it with spaces with "%20" to make sure no spaces are in the url
 			String url = burl.replaceAll(" ","%20");
+			//creating URL object with the url we created
 			URL obj = new URL(url);
 			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-			
+
+			//the request method is GET
 			con.setRequestMethod("GET");
+			//getting the response code
 			int respondCode = con.getResponseCode();
-			
+
+			//reading the response
 			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
 			String inputLine;
 			StringBuffer response = new StringBuffer();
@@ -30,9 +38,14 @@ public class API
 			}
 			in.close();
 			
-			
+			//store the response in to a JSONArray
 			JSONArray array = new JSONArray(response.toString());
-			
+
+			//check the response code
+			//if it is 200, there is no issue with the connection or anything
+			//and checking the JSONArray size to make sure there is a sensor by the specified id.
+			//if avaiable we are returning true
+			//else rturning false which means there are no sensors by the id
 			if(respondCode == 200 && array.length() > 0)
 			{
 				return true;
@@ -41,30 +54,42 @@ public class API
 		}
 		catch(Exception e)
 		{
+		    //if an exception occured we send the boolean false
 			return false;
 		}
 		
 	}
+	//update a sensor method
 	public boolean updateSensor(String id,boolean isActive,int co2Level,int smokeLevel)
 	{
+	    //getting id,active state,co2Level,smokeLevel as parameters
 		try
 		{
+		    //creating the url to send the request
 			String burl = "http://localhost:5000/api/sensors/updateSensorBySensorApp?id=" + id + "&co2Level=" + co2Level + "&smokeLevel=" + smokeLevel + "&isActive="+ isActive;
+			//replacing spaces with the "%20" to eleminate spaces in the url
 			String url = burl.replaceAll(" ","%20");
+			//creating a URl object with the url we created
 			URL obj = new URL(url);
 			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-			
+
+			//setting the request type (Here it is post)
 			con.setRequestMethod("POST");
 			int respondCode = con.getResponseCode();
-			
+
+			//if the response code is true
+			//the updates has been made successfully
+			//so we return true
 			if(respondCode == 200)
 			{
 				return true;
 			}
+			//else return false
 			return false;
 		}
 		catch(Exception e)
 		{
+		    //if an exception occured we return false as well
 			return false;
 		}
 		
--- Sensor-App/src/application/Main.java
+++ Sensor-App/src/application/Main.java
@@ -20,7 +20,8 @@ import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
-	
+
+	//all the UI elements
 	@FXML
 	private Label status;
 	@FXML
@@ -35,15 +36,19 @@ public class Main extends Application {
 	private Label response;
 	@FXML
 	private Label countLabel;
-	
+
+	//keeping a count to send the state of a sensor
 	private static int count = 10;
+	//storing the valid sensor id
 	private String sensorId;
+	//create an object of API class
 	API api = new API();
 	
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
+
+		//loading the UI
 		Parent root = FXMLLoader.load(getClass().getResource("UI.fxml"));
 		Scene scene = new Scene(root,500,500);
 		primaryStage.setScene(scene);
@@ -55,16 +60,24 @@ public class Main extends Application {
 	}
 	public void registerSensor(ActionEvent event) throws Exception
 	{
+	    //this method will be triggered when the register button is clicked
+	    //check whether the sensorIdField is entered or not
 		if(sensorIdField.getText().equals("") || sensorIdField.getText() == null)
 		{
+		    //if not showing an error
 			response.setText("Enter a valid sensor ID");
 			return;
 		}
 		
-		
+		//if the sensorIdField is successfully entered
+		//checking the entered id valid or not by calling a method in the API class
+		//if valid it will return true
 		boolean isValidId = api.checkSensorById(sensorIdField.getText().toString());
+		//checking the boolean value
 		if(isValidId)
 		{
+		    //if valid the registration is success
+		    //and it is ready to send its state
 			sensorIdField.setDisable(true);
 			registerButton.setText("Registered");
 			registerButton.setDisable(true);
@@ -74,23 +87,30 @@ public class Main extends Application {
 		}
 		else
 		{
+		    //else showing the error response
 			response.setText("Entered ID is not mapped to a sensor!");
 		}
 		
 		
 	}
+	//this method is reposnsible to update the sensor every 10 seconds
 	public void updateSensorDetails()
 	{
-		//send sensor status every 5 seconds
+		//send sensor status every 10 seconds
 		Timer timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
             public void run() 
             {
+                //decreasing the count variable
             	count--;
+            	//if the count == 0
+            	//it is the time to send to state of a sensor to thr API
             	if(count == 0)
             	{
+            	    //we make the count varibale 10 again to show the remaining time in the app
             		count = 10;
+            		//and this method will generate the sensor state
             		sendUpdates();
             	}
             	 Platform.runLater(() -> {
@@ -104,13 +124,14 @@ public class Main extends Application {
 	}
 	public void sendUpdates()
 	{
+	    //generating random values for active state,co2 level and smoke level
 		Random rand = new Random();
 		int smoke = rand.nextInt((10 - 1) + 1) + 1;
 		int co2 = rand.nextInt((10 - 1) + 1) + 1;
 		
 		Platform.runLater(() -> 
 		{
-			
+			//setting the UI elements with the randomly generated values
 			boolean isActive = false;
 			if((smoke + co2) > 10)
 			{
@@ -128,7 +149,8 @@ public class Main extends Application {
 			
 			co2Level.setText("CO2 Level : " + co2);
 			smokeLevel.setText("Smoke Level : " + smoke);
-			
+
+			//sending the values to the API with the randomly generated values
 			sendToAPI(isActive,smoke,co2);
 			
           });
@@ -137,9 +159,11 @@ public class Main extends Application {
 
 	private void sendToAPI(boolean isActive, int smoke, int co2)
 	{
+	    //calling the API class updateSensor method to update the sensor with the parameters
 		boolean isUpdated = api.updateSensor(sensorId, isActive, co2, smoke);
 		Platform.runLater(() -> 
 		{
+		    //check whether it is updated or not
 			if(isUpdated)
 			{
 				response.setText("Updates were successfully sent!");
